146fb5412c8ef2486de86e9c4aac834c0a8f724b
Mikhail A Pokidko <mikhail.pokidko@gmail.com>
2012-10-26 01:54:04 -0700

    Part3: suddenly

--- features/sort_movie_list.feature
+++ features/sort_movie_list.feature
@@ -23,9 +23,20 @@ Background: movies have been added to database
 
 Scenario: sort movies alphabetically
   When I follow "Movie Title"
-  # your steps here
+  Then I should see "2001: A Space Odyssey" before "Aladdin"
+  And I should see "Aladdin" before "Amelie"
+  And I should see "Amelie" before "Chicken Run"
+  And I should see "Chicken Run" before "Chocolat"
+  And I should see "Chocolat" before "The Help"
+  And I should see "The Help" before "The Terminator"
+  And I should see "The Terminator" before "When Harry Met Sally"
 
 Scenario: sort movies in increasing order of release date
   When I follow "Release Date"
-  # your steps here
-
+  Then I should see "1968" before "1984"
+  And I should see "1984" before "1989"
+  And I should see "1989" before "1992"
+  And I should see "1992" before "2000"
+  And I should see "2000" before "2001-01"
+  And I should see "2001-01" before "2002-04"
+  And I should see "2001-04" before "2011"
--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -15,7 +15,16 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.content  is the entire content of the page as a string.
-  flunk "Unimplemented"
+  #flunk "Unimplemented"
+#  index1 = page.body.index(e1)
+#  index2 = page.body.index(e2)
+#  if index1 == nil || index2 == nil
+#    false.should == true
+#  else
+#    index1.should < index2
+#  end
+  #page.text.should =~ /.*#{e1}.*#{e2}/m
+  /.*#{e1}.*#{e2}/.match(page.text)
 end
 
 # Make it easier to express checking or unchecking several boxes at once
@@ -26,4 +35,20 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+  rating_list.split().each do |rating|
+    step %{I #{uncheck ? "uncheck" : "check"} "ratings_#{rating}"}
+    #check("ratings[#{rating}]")
+  end
+end
+
+When /I (un)?check all the ratings/ do |uncheck|
+  Movie.all_ratings.each do |rating|
+    step %{I #{ uncheck ? "uncheck" : "check" } "ratings_#{rating}"}
+  end
+end
+
+Then /I should see (all|none) of the movies/ do |see|
+  Movie.select(:title).map(&:title).each do |movie|
+    step %Q{I should #{see == "none" ? "not" : ""} see "#{movie}"}
+  end
 end
